61f00edb93e7b1096d4292aaadd88ad47075c58d
marcprux <mwp1@cornell.edu>
2024-05-26 19:24:49 -0400

    Update formatting tests

--- Tests/SkipFoundationTests/Foundation/LocaleTests.swift
+++ Tests/SkipFoundationTests/Foundation/LocaleTests.swift
@@ -151,7 +151,16 @@ final class LocaleTests: XCTestCase {
         XCTAssertEqual("1,234", 1234.formatted(.number.locale(Locale(identifier: "en_US"))))
         XCTAssertEqual("1,234.567", 1234.567.formatted(.number.locale(Locale(identifier: "en_US"))))
 
-        // inconsistent due to different time zones
+        XCTAssertEqual("A, B, and C", ["A", "B", "C"].formatted())
+        XCTAssertEqual("1, 2.3 et 3.4567", [1, 2.3, 3.4567].formatted(.list(memberStyle: .number, type: .and).locale(Locale(identifier: "fr"))))
+        XCTAssertEqual("1、2.3、3.4567", [1, 2.3, 3.4567].formatted(.list(memberStyle: .number, type: .and).locale(Locale(identifier: "ja"))))
+        XCTAssertEqual("A, B, or C", ["A", "B", "C"].formatted(.list(type: .or)))
+        XCTAssertEqual("A, B ou C", ["A", "B", "C"].formatted(.list(type: .or).locale(Locale(identifier: "fr"))))
+
+        XCTAssertEqual("A、B、またはC", ["A", "B", "C"].formatted(.list(type: .or).locale(Locale(identifier: "ja"))))
+        XCTAssertEqual("A、B、C", ["A", "B", "C"].formatted(.list(type: .and).locale(Locale(identifier: "ja"))))
+
+        // inconsistent due to different time zones in CI runner
 
         //XCTAssertEqual("15:00", (Date(timeIntervalSince1970: 100)..<Date(timeIntervalSince1970: 1000)).formatted(.timeDuration))
         //
@@ -166,19 +175,10 @@ final class LocaleTests: XCTestCase {
         //XCTAssertEqual("19:01:40 EST", Date(timeIntervalSince1970: 100).formatted(date: .omitted, time: .complete))
         //XCTAssertEqual("19:01", Date(timeIntervalSince1970: 100).formatted(date: .omitted, time: .shortened))
         //XCTAssertEqual("19:01:40", Date(timeIntervalSince1970: 100).formatted(date: .omitted, time: .standard))
-
-        XCTAssertEqual("A, B, and C", ["A", "B", "C"].formatted())
-        XCTAssertEqual("1, 2.3 et 3.4567", [1, 2.3, 3.4567].formatted(.list(memberStyle: .number, type: .and).locale(Locale(identifier: "fr"))))
-        XCTAssertEqual("1、2.3、3.4567", [1, 2.3, 3.4567].formatted(.list(memberStyle: .number, type: .and).locale(Locale(identifier: "ja"))))
-        XCTAssertEqual("A, B, or C", ["A", "B", "C"].formatted(.list(type: .or)))
-        XCTAssertEqual("A, B ou C", ["A", "B", "C"].formatted(.list(type: .or).locale(Locale(identifier: "fr"))))
-
-        XCTAssertEqual("12/31/1969, 19:01、12/31/1969, 19:16", [Date(timeIntervalSince1970: 100), Date(timeIntervalSince1970: 1_000)].formatted(.list(memberStyle: .dateTime, type: .and).locale(Locale(identifier: "ja"))))
-        XCTAssertEqual("31/12/1969 19:01 et 31/12/1969 19:16", [Date(timeIntervalSince1970: 100), Date(timeIntervalSince1970: 1_000)].formatted(.list(memberStyle: .dateTime.locale(Locale(identifier: "fr")), type: .and).locale(Locale(identifier: "fr"))))
-        XCTAssertEqual("31/12/1969 19:01和31/12/1969 19:16", [Date(timeIntervalSince1970: 100), Date(timeIntervalSince1970: 1_000)].formatted(.list(memberStyle: .dateTime.locale(Locale(identifier: "fr")), type: .and).locale(Locale(identifier: "zh"))))
-
-        XCTAssertEqual("A、B、またはC", ["A", "B", "C"].formatted(.list(type: .or).locale(Locale(identifier: "ja"))))
-        XCTAssertEqual("A、B、C", ["A", "B", "C"].formatted(.list(type: .and).locale(Locale(identifier: "ja"))))
+        //
+        //XCTAssertEqual("12/31/1969, 19:01、12/31/1969, 19:16", [Date(timeIntervalSince1970: 100), Date(timeIntervalSince1970: 1_000)].formatted(.list(memberStyle: .dateTime, type: .and).locale(Locale(identifier: "ja"))))
+        //XCTAssertEqual("31/12/1969 19:01 et 31/12/1969 19:16", [Date(timeIntervalSince1970: 100), Date(timeIntervalSince1970: 1_000)].formatted(.list(memberStyle: .dateTime.locale(Locale(identifier: "fr")), type: .and).locale(Locale(identifier: "fr"))))
+        //XCTAssertEqual("31/12/1969 19:01和31/12/1969 19:16", [Date(timeIntervalSince1970: 100), Date(timeIntervalSince1970: 1_000)].formatted(.list(memberStyle: .dateTime.locale(Locale(identifier: "fr")), type: .and).locale(Locale(identifier: "zh"))))
         #endif
     }
 
